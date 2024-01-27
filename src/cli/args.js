187c30b3c5dd5f158b1897d88784a56ee3064920
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-27 17:14:20 +0500

    feat: solve cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+const SEPARATOR = ', ';
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = args
+    .reduce(
+      (acc, curr, i) =>
+        curr.startsWith('--') && args[i + 1] ? [...acc, `${curr.slice(2)} is ${args[i + 1]}`] : acc,
+      [],
+    )
+    .join(SEPARATOR);
+
+  console.log(parsedArgs);
 };
 
 parseArgs();
