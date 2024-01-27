f737349bd94df06659a3dcc198dd4d27505303a9
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-27 18:05:04 +0500

    feat: solve streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { resolve } from 'node:path';
+import { createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { getDirname } from '../utils/path/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+const FILE_PATH = resolve(__dirname, './files/fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  const wstream = createWriteStream(FILE_PATH, 'utf-8');
+
+  try {
+    await pipeline(process.stdin, wstream);
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : 'Failed to write file');
+  }
 };
 
 await write();
