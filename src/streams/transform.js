35f1b4a0ade900d1e2caffc2855184f148c30b34
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-27 18:13:00 +0500

    feat: solve streams:transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+  const tstream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().trim().split('').reverse().join('');
+
+      callback(null, reversedChunk + '\n');
+    },
+  });
+
+  try {
+    await pipeline(process.stdin, tstream, process.stdout);
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : 'Failed to transform data');
+  }
 };
 
 await transform();
