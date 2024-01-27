a7435ba4ea286ce44968561b304fd762a1af492a
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-27 15:13:27 +0500

    feat: solve fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { resolve } from 'node:path';
+import { writeFile } from 'node:fs/promises';
+import { getDirname } from '../utils/path/getDirname.js';
+import { FileSystemError } from '../utils/error/FileSystemError.js';
+
+const __dirname = getDirname(import.meta.url);
+const CONTENT = 'I am fresh and young';
+const DESTINATION = resolve(__dirname, './files/fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(DESTINATION, CONTENT, { flag: 'wx+' });
+  } catch {
+    throw new FileSystemError();
+  }
 };
 
 await create();
