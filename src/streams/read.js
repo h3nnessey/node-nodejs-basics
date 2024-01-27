4d02f957bdf35333cd81553859bd5bcbcf2fcf83
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-27 18:04:54 +0500

    feat: solve streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { resolve } from 'node:path';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { getDirname } from '../utils/path/getDirname.js';
+
+const __dirname = getDirname(import.meta.url);
+const FILE_PATH = resolve(__dirname, './files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const rstream = createReadStream(FILE_PATH, 'utf-8');
+
+  try {
+    await pipeline(rstream, process.stdout);
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : 'Failed to read file');
+  }
 };
 
 await read();
