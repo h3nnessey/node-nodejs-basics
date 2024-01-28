8147f6fe6384cd3437512df1486898c32a3af35d
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-28 14:53:20 +0500

    feat: solve fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { getPathToFile } from '../utils/path/getPathToFile.js';
+import { FileSystemError } from '../utils/error/FileSystemError.js';
+
+const FILE_PATH = getPathToFile(import.meta.url, './files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(FILE_PATH, 'utf-8');
+
+    console.log(content);
+  } catch {
+    throw new FileSystemError();
+  }
 };
 
 await read();
