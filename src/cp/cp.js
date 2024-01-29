bce3f56d4f32926ace9ef7ce7efcb00c4be94d06
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-29 18:06:14 +0500

    feat: solve cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'node:child_process';
+import { getResolvedPath } from '../utils/path/getResolvedPath.js';
+
+const SCRIPT_PATH = getResolvedPath(import.meta.url, './files/script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', [SCRIPT_PATH, ...args]);
+
+  // the stdin of the child process receives input data from the stdin of the master process
+  process.stdin.on('data', (chunk) =>
+    childProcess.stdin.write(chunk.toString().trim()),
+  );
+
+  // the stdout of the child process sends data to the stdout of the master process
+  childProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--write', '--some', '--arguments', '--here']);
