eda8adcbe7337d44e58b5056b92e3cb6af1800a8
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-29 23:54:26 +0500

    feat: add example of IPC

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,7 +5,9 @@ import { getResolvedPath } from '../utils/path/getResolvedPath.js';
 const SCRIPT_PATH = getResolvedPath(import.meta.url, './files/script.js');
 
 const spawnChildProcess = async (args) => {
-  const childProcess = spawn('node', [SCRIPT_PATH, ...args]);
+  const childProcess = spawn('node', [SCRIPT_PATH, ...args], {
+    stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+  });
 
   // the stdin of the child process receives input data from the stdin of the master process
   process.stdin.on('data', (chunk) =>
@@ -19,6 +21,9 @@ const spawnChildProcess = async (args) => {
   childProcess.on('exit', (code) =>
     process.stdout.write(`Child process terminated with code ${code + EOL}`),
   );
+
+  // send message to child process using IPC channel
+  childProcess.send('IPC MESSAGE FROM MASTER PROCESS');
 };
 
 spawnChildProcess(['--write', '--some', '--arguments', '--here']);
