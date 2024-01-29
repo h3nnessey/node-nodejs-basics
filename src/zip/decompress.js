e1621816856dc5f48670882d67b2acf21d21f6a4
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-29 15:16:18 +0500

    feat: solve zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createBrotliDecompress, constants } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { getResolvedPath } from '../utils/path/getResolvedPath.js';
+import { validateError } from '../utils/error/validateError.js';
+
+const { url } = import.meta;
+const SOURCE_PATH = getResolvedPath(url, './files/archive.gz');
+const DESTINATION_PATH = getResolvedPath(url, './files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const brotliDecompress = createBrotliDecompress();
+
+  const readStream = createReadStream(SOURCE_PATH);
+  const writeStream = createWriteStream(DESTINATION_PATH);
+
+  try {
+    await pipeline(readStream, brotliDecompress, writeStream);
+  } catch (error) {
+    console.error(validateError(error, 'Failed to decompress file'));
+  }
 };
 
 await decompress();
