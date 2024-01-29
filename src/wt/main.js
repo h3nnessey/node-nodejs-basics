ac4fc1402d21ac470e46b33e4684f363f409b3e4
Vitaly <vitaliy.zayakin@gmail.com>
2024-01-29 16:18:10 +0500

    feat: solve wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+import { getResolvedPath } from '../utils/path/getResolvedPath.js';
+
+const WORKER_PATH = getResolvedPath(import.meta.url, './worker.js');
+const INITIAL_N = 10;
+
+const createWorker = (workerPath, workerData) =>
+  new Promise((resolve, reject) => {
+    const worker = new Worker(workerPath, { workerData });
+
+    worker.on('message', resolve);
+    worker.on('error', reject);
+  });
+
+const mapWorkerResults = (results) =>
+  results.map(({ status, value }) => {
+    const isFailed = status === 'rejected';
+
+    return {
+      status: isFailed ? 'error' : 'resolved',
+      data: isFailed ? null : value,
+    };
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = cpus().map((_, index) =>
+    createWorker(WORKER_PATH, { n: INITIAL_N + index }),
+  );
+
+  const workerResults = await Promise.allSettled(workers);
+  const mappedResults = mapWorkerResults(workerResults);
+
+  console.log(mappedResults);
 };
 
 await performCalculations();
