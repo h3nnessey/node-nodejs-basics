7136b20cbbbb89f0754f1d6b6ae4bede346a9839
Vitaly <vitaliy.zayakin@gmail.com>
2024-10-08 00:06:40 +0500

    fix: fix json import

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,8 +1,8 @@
-import path from 'node:path';
-import { release, version } from 'node:os';
-import { createServer as createServerHttp } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import './files/c.js';
+import path from "node:path";
+import { release, version } from "node:os";
+import { createServer as createServerHttp } from "node:http";
+import { fileURLToPath } from "node:url";
+import "./files/c.js";
 
 /* For Student 1: we also can create a custom require to import json files
 import { createRequire } from 'node:module';
@@ -14,10 +14,12 @@ const random = Math.random();
 let unknownObject;
 
 if (random > 0.5) {
-  unknownObject = (await import('./files/a.json', { assert: { type: 'json' } })).default;
+  unknownObject = (await import("./files/a.json", { with: { type: "json" } }))
+    .default;
   // unknownObject = require('./files/a.json')
 } else {
-  unknownObject = (await import('./files/b.json', { assert: { type: 'json' } })).default;
+  unknownObject = (await import("./files/b.json", { with: { type: "json" } }))
+    .default;
   // unknownObject = require('./files/b.json')
 }
 
@@ -31,7 +33,7 @@ console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-  res.end('Request accepted');
+  res.end("Request accepted");
 });
 
 const PORT = 3000;
@@ -40,7 +42,7 @@ console.log(unknownObject);
 
 myServer.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-  console.log('To terminate it, use Ctrl+C combination');
+  console.log("To terminate it, use Ctrl+C combination");
 });
 
 export { unknownObject, myServer };
